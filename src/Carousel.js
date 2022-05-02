124c02280e46fe4c16038a4c7c297bad8fb90d17
o0pmitev <pmitev.dev@gmail.com>
2022-05-02 07:05:20 +0200

    :sparkles: Initial implementation of Carousel component

---
+++ src/Carousel.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import CarouselButton from './CarouselButton';
+import CarouselSlide from './CarouselSlide';
+
+class Carousel extends React.PureComponent {
+  static propTypes = {
+    slides: PropTypes.arrayOf(PropTypes.shape(CarouselSlide.propTypes))
+      .isRequired,
+  };
+
+  state = {
+    slideIndex: 0,
+  };
+
+  handlePrevClick = () => {
+    const { slides } = this.props;
+    this.setState(({ slideIndex }) => ({
+      slideIndex: (slideIndex + slides.length - 1) % slides.length, // 1
+    }));
+  };
+
+  handleNextClick = () => {
+    const { slides } = this.props;
+    this.setState(({ slideIndex }) => ({
+      slideIndex: (slideIndex + 1) % slides.length,
+    }));
+  };
+
+  render() {
+    const { slides, ...rest } = this.props;
+    return (
+      <main {...rest}>
+        <CarouselSlide {...slides[this.state.slideIndex]} />
+        <CarouselButton data-action="prev" onClick={this.handlePrevClick}>
+          Prev
+        </CarouselButton>
+        <CarouselButton data-action="next" onClick={this.handleNextClick}>
+          Next
+        </CarouselButton>
+      </main>
+    );
+  }
+}
+
+export default Carousel;
